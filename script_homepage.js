89269d57c735bb4ad8a98c81c4897b57d7686889
BillyBoy <clem.nourricier@hotmail.fr>
2023-03-29 11:49:24 +0200

    Ajout de projet fonctionnel

--- script_homepage.js
+++ script_homepage.js
@@ -2,9 +2,10 @@ let categories = []
 let works = []
 let worksBackup = []
 
+// ----------------- GESTION DE LA PAGE D'ACCUEIL -----------------  //
+
 // Cette fonction affiche les oeuvres en fonction de la catégorie sélectionnée
 const displayWorks = category => {
-  // On sélectionne l'élément HTML qui contiendra les oeuvres
   const portfolio = document.querySelector('.gallery')
   // On vide cet élément pour supprimer les oeuvres précédemment affichées
   portfolio.innerHTML = ''
@@ -32,7 +33,6 @@ const displayWorks = category => {
 
 // Cette fonction affiche les boutons de sélection de catégorie
 const filterCategories = () => {
-  // On sélectionne l'élément HTML qui contiendra les boutons
   const divCategories = document.querySelector('.filter')
 
   // On ajoute un bouton "Tous" en premier (grâce à unshift qui ajoute l'élément et décale les autres éléments du tableau à droite)
@@ -41,36 +41,30 @@ const filterCategories = () => {
 
   // On crée un bouton pour chaque catégorie
   categories.forEach(category => {
-    const buttonCategories = document.createElement('button')
-    buttonCategories.innerText = category.name
-    buttonCategories.classList.add('button-style')
+    const categoryButton = document.createElement('button')
+    categoryButton.innerText = category.name
+    categoryButton.classList.add('button-style')
 
     // On ajoute la classe "active" au bouton "Tous" (id: 0 étant "Tous")
     if (category.id === 0) {
-      buttonCategories.classList.add('active')
+      categoryButton.classList.add('active')
     }
 
-    // On ajoute chaque bouton au HTML
-    divCategories.appendChild(buttonCategories)
+    divCategories.appendChild(categoryButton)
 
-    // On ajoute un événement "click" à chaque bouton
-    buttonCategories.addEventListener('click', () => {
+    categoryButton.addEventListener('click', () => {
       // On filtre les oeuvres à afficher en fonction de la catégorie sélectionnée
       works =
         category.id === 0
           ? [...worksBackup] // Si 'Tous' est sélectionné, on affiche toutes les oeuvres
           : worksBackup.filter(work => work.categoryId === category.id) // Sinon, on affiche seulement les oeuvres de la catégorie sélectionnée
 
-      // On supprime la classe "active"
+      // On met la classe "active" au filtre sélectionné
       const activeButton = document.querySelector('.active')
       if (activeButton) {
         activeButton.classList.remove('active')
       }
-
-      // On ajoute la classe "active" au bouton sélectionné
-      buttonCategories.classList.add('active')
-
-      // On affiche les oeuvres correspondant à la catégorie sélectionnée
+      categoryButton.classList.add('active')
       displayWorks(category.name)
     })
   })
@@ -79,19 +73,17 @@ const filterCategories = () => {
 // Cette fonction récupère les données des catégories et des oeuvres depuis une API
 const fetchData = async () => {
   try {
-    // Récupération de deux réponses en parallèle en utilisant Promise.all
     const [categoriesResponse, worksResponse] = await Promise.all([
       fetch('http://localhost:5678/api/categories'),
       fetch('http://localhost:5678/api/works')
     ])
 
-    // Si la réponse pour les catégories n'est pas réussie, une erreur est levée
+    // Si la réponsen'est pas réussie, une erreur est levée
     if (!categoriesResponse.ok) {
       throw new Error(
         `Failed to fetch categories: ${categoriesResponse.status}`
       )
     }
-    // Si la réponse pour les œuvres n'est pas réussie, une erreur est levée
     if (!worksResponse.ok) {
       throw new Error(`Failed to fetch works: ${worksResponse.status}`)
     }
@@ -112,8 +104,6 @@ const fetchData = async () => {
     console.error(e)
   }
 }
-
-// La fonction fetchData est appelée pour récupérer les données au démarrage de l'application
 fetchData()
 
 // ----------------- MODALE -----------------  //
@@ -121,9 +111,6 @@ fetchData()
 // Sélection des éléments du DOM
 const openModalContainer = document.querySelector('#open-modal-container')
 const openModalButton = document.querySelector('#open-modal')
-const closeDeleteModalButton = document.querySelector(
-  '#close-delete-project-modal'
-)
 const closeAddModalButton = document.querySelector('#close-add-project-modal')
 const deleteProjectModal = document.getElementById('delete-project-modal')
 const addProjectModal = document.getElementById('add-project-modal')
@@ -131,51 +118,73 @@ const addProjectButton = document.querySelector('.addPictures')
 const modalGallery = document.getElementById('modal-gallery')
 const modifPortfolio = document.getElementById('modif-portfolio')
 const modifPic = document.getElementById('modif-pic')
-const loginLink = document.querySelector('#nav-login')
-const logoutLink = document.createElement('li')
 const filter = document.querySelector('.filter')
-const returnButton = document.getElementById('return')
-logoutLink.textContent = 'logout'
 
-returnButton.addEventListener('click', () => {
-  addProjectModal.close()
+// Vérification de la connexion utilisateur
+function loginLogout() {
+  const token = localStorage.getItem('token')
+  const logoutLink = document.getElementById('logoutLink')
+  const loginLink = document.getElementById('loginLink')
+
+  if (token) {
+    logoutLink.style.display = 'block'
+    loginLink.style.display = 'none'
+    logoutLink.addEventListener('click', () => {
+      localStorage.removeItem('token')
+      window.location.reload()
+    })
+  } else {
+    logoutLink.style.display = 'none'
+    loginLink.style.display = 'block'
+  }
+}
+
+// Gestionnaire d'événements pour ouvrir la modale
+openModalButton.addEventListener('click', () => {
   deleteProjectModal.showModal()
+  displayWorksInModal()
+  document.body.classList.add('modal-open')
 })
 
-closeDeleteModalButton.addEventListener('click', function () {
-  modal.close()
+// Fonction pour fermer toutes les modales
+function closeModals() {
+  deleteProjectModal.close()
+  addProjectModal.close()
   document.body.classList.remove('modal-open')
-})
+}
+
 // Ferme la modale si l'utilisateur clique en dehors de celle-ci
 function closeOnClickOutside(modal) {
   window.addEventListener('click', function (event) {
-    if (
-      event.target == modal ||
-      event.target == closeDeleteModalButton ||
-      event.target == closeAddModalButton
-    ) {
+    if (event.target == modal || event.target == closeAddModalButton) {
       closeModals()
       document.body.classList.remove('modal-open')
     }
   })
 }
 
+// Vérification de la présence d'un token d'authentification dans le local storage
+const isConnected = localStorage.getItem('token') !== null
+
+if (isConnected) {
+  openModalContainer.style.display = 'block'
+  filter.style.display = 'none'
+  showModifIcons()
+  loginLogout()
+} else {
+  hideModifIcons()
+}
+
+// Fonction pour afficher les icônes d'édition des travaux
 function showModifIcons() {
   modifPortfolio.classList.remove('hidden')
   modifPic.classList.remove('hidden')
 }
 
+// Fonction pour cacher les icônes d'édition des travaux
 function hideModifIcons() {
   modifPortfolio.classList.add('hidden')
   modifPic.classList.add('hidden')
-}
-
-function loginLogout() {
-  loginLink.parentNode.replaceChild(logoutLink, loginLink)
-  logoutLink.addEventListener('click', () => {
-    localStorage.removeItem('token')
-    window.location.reload()
-  })
 }
 
 // Fonction pour afficher les travaux dans la modale
@@ -230,6 +239,7 @@ function displayWorksInModal() {
           })
       }
     })
+
     // Créer l'image et l'ajouter au conteneur
     const modalImg = document.createElement('img')
     modalImg.src = work.imageUrl
@@ -259,47 +269,145 @@ function displayWorksInModal() {
     modalGallery.appendChild(modalFigure)
   })
 }
-
-// Vérification de la présence d'un token d'authentification dans le local storage
-const isConnected = localStorage.getItem('token') !== null
-
-if (isConnected) {
-  openModalContainer.style.display = 'block'
-  filter.style.display = 'none'
-  showModifIcons()
-  loginLogout()
-} else {
-  hideModifIcons()
-}
-
-// Gestionnaire d'événements pour ouvrir la modale
-openModalButton.addEventListener('click', () => {
-  deleteProjectModal.showModal()
-  displayWorksInModal()
-  document.body.classList.add('modal-open')
-})
-
 displayWorksInModal()
-closeOnClickOutside(deleteProjectModal, closeDeleteModalButton)
+closeOnClickOutside(deleteProjectModal)
 
-// Modale d'ajout //
-
-function openProjectModal() {
-  deleteProjectModal.showModal()
-}
-
-function openAddProjectModal() {
-  addProjectModal.showModal()
-  deleteProjectModal.close() // ferme la modale 1 si elle est ouverte
-}
-
-function closeModals() {
-  deleteProjectModal.close()
-  addProjectModal.close()
-}
-
+// Ajoute un événement au clic sur le bouton d'ajout de projet pour ouvrir la modale d'ajout
 addProjectButton.addEventListener('click', () => {
   openAddProjectModal()
 })
 
+// Ferme la modale d'ajout de projet si l'utilisateur clique en dehors de la modale ou sur le bouton de fermeture
 closeOnClickOutside(addProjectModal, closeAddModalButton)
+
+const inputElement = document.getElementById('image')
+const previewElement = document.getElementById('image-preview')
+const addButtonElement = document.querySelector('.add-picture-button')
+const addPicturePlaceholderElement = document.querySelector(
+  '.add-picture-placeholder'
+)
+
+inputElement.addEventListener('change', e => {
+  const file = e.target.files[0]
+
+  if (file) {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+
+    reader.onload = () => {
+      previewElement.src = reader.result
+      addPicturePlaceholderElement.style.display = 'none'
+      previewElement.classList.remove('hidden')
+    }
+  } else {
+    previewElement.src = ''
+    addPicturePlaceholderElement.style.display = 'block'
+    previewElement.classList.add('hidden')
+  }
+})
+
+// Récupération des éléments du DOM
+const addProjectForm = document.getElementById('add-project-form')
+const addPictureButton = document.querySelector('.add-picture-button')
+const imageInput = document.getElementById('image')
+const imagePreview = document.getElementById('image-preview')
+const pictureTitleInput = document.getElementById('title')
+const categorySelection = document.getElementById('category-selection')
+
+// Fonction pour afficher la modale d'ajout de projet
+function openAddProjectModal() {
+  // Vérifie si la modale est déjà ouverte
+  if (!addProjectModal.hasAttribute('open')) {
+    // Affichage de la modale
+    addProjectModal.showModal()
+    // Ajout des options de catégories à la liste déroulante
+    fetch('http://localhost:5678/api/categories')
+      .then(response => response.json())
+      .then(categories => {
+        categories.forEach(category => {
+          const option = document.createElement('option')
+          option.value = category.id
+          option.textContent = category.name
+          categorySelection.appendChild(option)
+        })
+      })
+  }
+}
+
+// Fonction pour fermer la modale d'ajout de projet
+function closeAddProjectModal() {
+  // Réinitialisation du formulaire
+  addProjectForm.reset()
+  imagePreview.classList.add('hidden')
+  imagePreview.src = ''
+  categorySelection.innerHTML = ''
+
+  // Fermeture de la modale
+  addProjectModal.close()
+}
+
+// Fonction pour prévisualiser l'image sélectionnée
+function previewImage() {
+  const file = imageInput.files[0]
+  const reader = new FileReader()
+
+  reader.addEventListener('load', () => {
+    imagePreview.src = reader.result
+    imagePreview.classList.remove('hidden')
+  })
+
+  if (file) {
+    reader.readAsDataURL(file)
+  }
+}
+
+// Gestionnaire d'événement pour ouvrir la modale d'ajout de projet
+document
+  .querySelector('.addPictures')
+  .addEventListener('click', openAddProjectModal)
+
+// Gestionnaire d'événement pour fermer la modale d'ajout de projet
+document
+  .getElementById('close-add-project-modal')
+  .addEventListener('click', closeAddProjectModal)
+
+// Gestionnaire d'événement pour retourner à la modale de galerie
+document.getElementById('return').addEventListener('click', () => {
+  closeAddProjectModal()
+})
+
+// Gestionnaire d'événement pour prévisualiser l'image sélectionnée
+imageInput.addEventListener('change', previewImage)
+
+// Gestionnaire d'événement pour valider le formulaire d'ajout de projet
+addProjectForm.addEventListener('submit', event => {
+  event.preventDefault()
+
+  // Envoi de la requête POST pour ajouter le projet
+  const formData = new FormData(addProjectForm)
+
+  fetch('http://localhost:5678/api/works/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    },
+
+    body: formData
+  })
+    .then(response => {
+      if (response.ok) {
+        // Fermeture de la modale et rechargement de la page pour afficher le nouveau projet
+        closeAddProjectModal()
+        location.reload()
+      } else {
+        throw new Error("Erreur lors de l'ajout du projet")
+      }
+    })
+    .catch(error => {
+      console.error(error)
+      const errorMessage = document.getElementById('error-message')
+      errorMessage.innerHTML =
+        "Une erreur s'est produite. Veuillez vérifier les champs du formulaire et réessayer."
+    })
+})
